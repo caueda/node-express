db5de2690a090ad1f7d2907190fe3aba8522ca86
Carlos R. Ueda <carlos.ueda@gmail.com>
2023-02-12 16:54:11 -0300

    Adding uuid to generate a unique identifier

--- routes/controllers/places-controller.js
+++ routes/controllers/places-controller.js
@@ -1,4 +1,5 @@
 const HttpError = require('../models/http-error');
+const uuid = require('uuid');
 
 const DUMYY_PLACES = [
     {
@@ -36,6 +37,7 @@ const getPlaceByUserId = (req, res, next) => {
 const createPlace = (req, res, next) => {
     const { title, description, coordinates, address, creator } = req.body;
     const createdPlace = {
+        id: uuid.v4(),
         title,
         description,
         location: coordinates,
